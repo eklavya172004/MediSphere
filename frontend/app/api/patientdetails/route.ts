ee2b19057bbaa9c5fd3c9be48646562029d39772
Eklavya Nath <optimusprime172004@gmail.com>
2025-04-21 00:02:34 +0530

    middleware

--- frontend/app/api/patientdetails/route.ts
+++ frontend/app/api/patientdetails/route.ts
@@ -5,7 +5,7 @@ import { NextResponse } from "next/server";
 export async function GET() {
 
 
-  const cookieStore =await  cookies();
+  const cookieStore =  cookies();
   const supabase = createRouteHandlerClient({
     cookies:  () => (cookieStore),  // Wrap in Promise.resolve()
   });
